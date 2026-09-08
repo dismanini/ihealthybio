bd51e83dcfae01cb846e4cb0a753d051c550a345
dismanini <dismanini@gmail.com>
2026-09-08 17:40:29 +0530

    first commit

---
+++ app/cache/min/1/app/plugins/gutentoc-advance-table-of-content/dist/toc6d6d.js
@@ -0,0 +1,4 @@
+(function($){$(document).ready(function(){var toggle=$('#close');if(!toggle.data('visible')){$('#toclistclose').hide()}
+toggle.click(function(){if($(this).data('visible')){$(this).data('visible',!1);$('#toclistclose').hide('fast');$('#close').text('show')}else{$(this).data('visible',!0);$('#toclistclose').show('fast');$('#close').text('hide')}});var toggle=$('#open');if(!toggle.data('visible')){$('#toclist').show()}
+toggle.click(function(){if($(this).data('visible')){$(this).data('visible',!1);$('#toclist').show('fast');$('#open').text('hide')}else{$(this).data('visible',!0);$('#toclist').hide('fast');$('#open').text('show')}});$('body .gutentoc  a[href*="#"]').on('click',function(e){e.preventDefault()
+$('html, body').animate({scrollTop:$($(this).attr('href')).offset().top-120,},500,'linear')});$('body .scroll_id_show  a[href*="#"]').on('click',function(){$('html, body').animate({scrollTop:$($(this).attr('href')).offset().top-120,},500,'linear')})})})(jQuery)
